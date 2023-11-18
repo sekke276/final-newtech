6bebc0a45d988fdf5f94070119f14eac16b99dbb
sekke276 <tringuyen2762001@gmail.com>
2023-11-18 15:46:30 +0700

    remove db link

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 // Update below to match your own MongoDB connection string.
-const MONGO_URL =
-  "mongodb+srv://chothien:UxNZQqSftgVO7Agp@cluster0.qd7iqj2.mongodb.net/library?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
